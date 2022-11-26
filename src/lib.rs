199ab9030c6a3d4d27404e9e6e264070868ef064
ahqrt <1217223113@qq.com>
2022-11-26 13:10:05 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+fn load_image_from_array(_array: &[u8]) -> Dyna {}
